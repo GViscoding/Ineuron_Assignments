336309bd5acbc0aca93e63103d23460200b9f88b
GViscoding <gautamvelu01@gmail.com>
2022-10-28 15:20:02 +0530

    Pattern-1

--- AssignWAPpattern1.java
+++ AssignWAPpattern1.java
@@ -2,29 +2,29 @@
 public class AssignWAPpattern1 {
 
 	public static void main(String[] args) {
-		int n=20;
-		for (int j=0;j<n;j++){
-			
-			
-			for(int i=0;i<n;i++){
-				if(i==0 ||
-						i==(n-1) ||
-						j==(n-1) || 
-						j==0 ||
-						i+j<=(n-1)/2 ||
-						i-j>=(n-1)/2){ 
-					System.out.print("*");
+		int n=15;
+
+			for (int i=0;i<n;i++){
+				for(int j=0;j<n;j++){
+					if(j==0 ||
+							j==(n-1) ||
+							i==(n-1) || 
+							i==0 ||
+							i+j<=(n-1)/2 ||
+							j-i>=(n-1)/2){ 
+						System.out.print("*");
+					}
+					else {
+						System.out.print(" ");
+					}
+				
 				}
-				else {
-					System.out.print(" ");
-				}
-			
+				
+				System.out.println();
+				
 			}
-			
-			System.out.println();
-			
-		}
 
 	}
 	
 }
+
